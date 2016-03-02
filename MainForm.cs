cc0ffc38e6ecc54b8f39a1c61e492973ad701498
Chyifu <t102378056@ntut.org.tw>
2016-03-02 15:26:43 +0800

    main form

---
+++ MainForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.Util;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+
+namespace Final_Project
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolStripMenuItem_DIP_Click(object sender, EventArgs e)
+        {
+            DIP DIP_process = new DIP();
+            DIP_process.Show();
+            DIP_process.MdiParent = this;
+        }
+
+        private void ToolStripMenuItem_DIPwork_Click(object sender, EventArgs e)
+        {
+            DIPwork DIPwork_process = new DIPwork();
+            DIPwork_process.Show();
+            DIPwork_process.MdiParent = this;
+        }
+
+        private void ToolStripMenuItem_train_Click(object sender, EventArgs e)
+        {
+            train train_process = new train();
+            train_process.Show();
+            train_process.MdiParent = this;
+        }
+    }
+}
